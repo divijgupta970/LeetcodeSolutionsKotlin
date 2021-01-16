9d4fbdd66901bc8d091cd7bb2c1c625bb2a56bfa
Divij Gupta <divijgupta66@gmail.com>
2021-01-16 12:17:04 +0530

    21 added

---
+++ Problem21.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun mergeTwoLists(l1: ListNode?, l2: ListNode?): ListNode? {
+        if (l1 == null)
+            return l2
+        if (l2 == null)
+            return l1
+        return if (l1.`val` < l2.`val`) {
+            l1.next = mergeTwoLists(l1.next, l2)
+            l1
+        } else {
+            l2.next = mergeTwoLists(l2.next, l1)
+            l2
+        }
+    }
+}
